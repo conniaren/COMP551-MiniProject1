5ed9a5719c8c4e515ea732a5e673a3a9c700dadb
conniaren <conniaren@hotmail.com>
2020-10-18 00:55:27 -0400

    updated the data preprocessing to normalize across regions, removed more unnecessary features

--- main.py
+++ main.py
@@ -8,14 +8,32 @@ import matplotlib.pyplot as plt
 ##################################### Task 1 #####################################
 
 # Load data directly into an array (2D). This includes the header.
-search_data = np.genfromtxt('2020_US_weekly_symptoms_dataset.csv', delimiter=',', dtype=None, encoding='utf-8')
+search_data = np.genfromtxt('2020_US_weekly_symptoms_dataset.csv', delimiter=',')
+search_data_info = np.genfromtxt('2020_US_weekly_symptoms_dataset.csv', delimiter=',', dtype=None, encoding='utf-8')
 hospitalization_data = np.genfromtxt('aggregated_cc_by.csv', delimiter=',', dtype=None, encoding='utf-8')
 
-search_data_no_head = np.delete(search_data, 0, axis=0)
-hospitalization_data_no_head = np.delete(hospitalization_data, 0, axis=0)
+search_data_no_head = search_data[1:, :]
+hospitalization_data_no_head = hospitalization_data[1:, :]
+search_data_name = search_data_info[:, 0:8]
+search_data_symptoms = search_data_info[0, 9:]
 
-# Removing all symptoms with absolutely no search data, and subregion 2 columns
-search_data = np.delete(search_data, np.all(search_data_no_head == '', axis=0), axis=1)
+# Removing all symptoms with fewer than 50% filled data columns
+search_data = np.delete(search_data_no_head, np.count_nonzero(np.isnan(search_data_no_head), axis=0) >
+                        0.5 * np.shape(search_data_no_head)[0], axis=1)
+search_data_symptoms = search_data_symptoms[~(np.count_nonzero(np.isnan(search_data_no_head[:, 9:]), axis=0) >
+                                              0.5 * np.shape(search_data_no_head)[0])]
+
+# Normalization based on de-medianing the data
+search_data[np.isnan(search_data)] = 0
+
+median = np.median(search_data, axis=0)# MEDIAN ARRAY
+for i in range(search_data.shape[1]):
+    search_data[:, i] = search_data[:, i] - median[i]
+
+search_data_with_head = np.concatenate(([search_data_symptoms], search_data), axis=0)
+search_data_name = np.delete(search_data_name, [5, 6], axis=1)
+
+clean_search_data = np.concatenate((search_data_name, search_data_with_head), axis=1)
 
 '''
 Cleaning the hospitalization data. This was harder than just removing the empty columns in the search data.
@@ -36,17 +54,17 @@ for state in american_states:
     state_data = np.delete(hospitalization_data, ~(np.any(hospitalization_data == covid_region_code, axis=1)), axis=0)
     us_hospitalization_data = np.append(us_hospitalization_data, state_data, axis=0)
 
-us_hospitalization_data = np.delete(us_hospitalization_data, 0, axis=0)
+us_hospitalization_data = us_hospitalization_data[1:, :]
 
 # Remove states with no hospitalization data
-search_data = np.delete(search_data, np.any(search_data == "US-WV", axis=1), axis=0)
-search_data = np.delete(search_data, np.any(search_data == "US-DC", axis=1), axis=0)
-search_data = np.delete(search_data, np.any(search_data == "US-DE", axis=1), axis=0)
+clean_search_data = np.delete(clean_search_data, np.any(clean_search_data == "US-WV", axis=1), axis=0)
+clean_search_data = np.delete(clean_search_data, np.any(clean_search_data == "US-DC", axis=1), axis=0)
+clean_search_data = np.delete(clean_search_data, np.any(clean_search_data == "US-DE", axis=1), axis=0)
 # Remove dates with no hospitalization data
 remove_dates = ['01-06', '01-13', '01-20', '01-27', '02-03', '02-10', '02-17', '02-24']
 for date in remove_dates:
     full_date = f"2020-{date}"
-    search_data = np.delete(search_data, np.any(search_data == full_date, axis=1), axis=0)
+    clean_search_data = np.delete(clean_search_data, np.any(clean_search_data == full_date, axis=1), axis=0)
 
 # Merge daily data into weekly data based on the prior Monday of the week using Dataframes
 us_hospitalization_data = pd.DataFrame(us_hospitalization_data, columns=['open_covid_region_code', 'sub_region_1',
@@ -73,33 +91,35 @@ weekly_hospitalization_data = weekly_hospitalization_data.drop(weekly_hospitaliz
                                                                index).reset_index()
 
 # Merge the datasets together, provided both a numpy array and Dataframe version of it
-search_dataframe = pd.DataFrame(search_data[1:, :], columns=search_data[0, :])
+search_dataframe = pd.DataFrame(clean_search_data[1:, :], columns=clean_search_data[0, :])
 
-final_covid_dataframe = pd.concat([search_dataframe,weekly_hospitalization_data[['hospitalized_new',
-                                                                                 'hospitalized_cumulative']]],
-                                 axis=1)
+final_covid_dataframe = pd.concat([search_dataframe, weekly_hospitalization_data[['hospitalized_new',
+                                                                                  'hospitalized_cumulative']]],
+                                  axis=1)
 
-final_covid_dataset = np.append(search_data[1:, :], np.array(weekly_hospitalization_data
-                                                                            [['hospitalized_new',
-                                                                              'hospitalized_cumulative']]), axis=1)
+final_covid_dataset = np.append(clean_search_data[1:, :], np.array(weekly_hospitalization_data
+                                                                   [['hospitalized_new',
+                                                                     'hospitalized_cumulative']]), axis=1)
 
-print(final_covid_dataset)
-print(final_covid_dataframe)
-#final_covid_dataframe.to_csv('us_covid_dataset_final.csv')
+print(final_covid_dataset[1:, 6:127])
+print(final_covid_dataframe.iloc[:, 6:126].astype(float))
+print(final_covid_dataset.shape)
+
+# final_covid_dataframe.to_csv('us_covid_dataset_final.csv')
 
 ##################################### Task 2 #####################################
 
+
 # Visualize the evolution of popularity of various symptoms across different regions over time
 for col in final_covid_dataframe.columns:
-        if 'symptom:' in col:
-                df = final_covid_dataframe[["sub_region_1_code", "date", col]]
-                pivot_df = df.pivot(index='date', columns='sub_region_1_code', values=col).replace(np.nan, 0)
-                pivot_df_percent = pivot_df.apply(lambda x: x if x.name == 'date' else x*100/sum(x), axis=1)
+    if 'symptom:' in col:
+        df = final_covid_dataframe[["sub_region_1_code", "date", col]]
+        pivot_df = df.pivot(index='date', columns='sub_region_1_code', values=col).replace(np.nan, 0)
+        pivot_df_percent = pivot_df.apply(lambda x: x if x.name == 'date' else x * 100 / sum(x), axis=1)
 
-                pivot_df_percent.plot.bar(stacked=True)
-                plt.ylim(0, 100)
-                plt.ylabel("Searches (%)")
-                plt.title('Searches of {}'.format(col))
-                plt.legend(loc="center right", bbox_to_anchor=(1.13, 0.5), ncol=1)
-                #plt.show()
-
+        pivot_df_percent.plot.bar(stacked=True)
+        plt.ylim(0, 100)
+        plt.ylabel("Searches (%)")
+        plt.title('Searches of {}'.format(col))
+        plt.legend(loc="center right", bbox_to_anchor=(1.13, 0.5), ncol=1)
+        # plt.show()
